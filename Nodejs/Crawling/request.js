4d0974a871d185870b0ab3f766dfa0d8f1392ed1
ww8007 <wshmin@naver.com>
2021-03-27 21:35:43 +0900

    크롤링 개요

--- Nodejs/Crawling/request.js
+++ Nodejs/Crawling/request.js
@@ -1,9 +1,8 @@
-const request = require('request');
+const request = require("request");
 
 // 네이버 주소 가져오기
 const url = "https://www.naver.com";
 
-request(url, (error, response, body) => {  
-    console.log(body);
+request(url, (error, response, body) => {
+  console.log(response);
 });
-    
