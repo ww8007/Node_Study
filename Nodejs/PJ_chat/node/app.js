9e0dfb46d12dc527507aeb7f644bf227de5547c6
ww8007 <wshmin@naver.com>
2021-04-15 14:28:05 +0900

    WebPage Socket io On

--- Nodejs/PJ_chat/node/app.js
+++ Nodejs/PJ_chat/node/app.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-
-app.get('/', ( _ ,res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (_, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen( port, () => {
-    console.log('Express listening on port', port);
+const server = app.listen(port, () => {
+  console.log("Express listening on port", port);
+});
+
+const listen = require("socket.io");
+const io = listen(server);
+
+io.on("connection", (socket) => {
+  console.log("소캣 io 통신");
+  socket.on("client message", (data) => {
+    console.log(data);
+    io.emit("server message", {
+      message: data.message,
+    });
+  });
 });
