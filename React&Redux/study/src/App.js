7bb6c2a7eaa944137a858f1fc14df69426fb3fd1
ww8007 <wshmin@naver.com>
2021-05-12 00:11:13 +0900

    REACT 소개

--- React&Redux/study/src/App.js
+++ React&Redux/study/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const data = [
+  { title: "Node", value: 0 },
+  { title: "React", value: 1 },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      {data.map((id) => {
+        <>
+          <p key={id.value}>{id.title}</p>
+        </>;
+      })}
     </div>
   );
 }
